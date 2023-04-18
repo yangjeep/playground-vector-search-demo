dddcc10dfeabed79f4d2d4b2c0ee350a2ca4b2e2
Jiajian Yang <jiajian.yang@searchspring.com>
2023-04-17 22:56:20 -0400

    dumped firex based flow for openai api embedding

--- play.py
+++ play.py
@@ -1,6 +1,35 @@
 from getpass import getuser
+from firexkit.argument_conversion import SingleArgDecorator
+from firexkit.task import FireXTask
+
 from firexapp.engine.celery import app
+from firexapp.submit.arguments import InputConverter
+from dotenv import load_dotenv
+import os
+import openai
+
+
+def read_env_file():
+    # Load the .env file
+    load_dotenv()
+
+    # Get the value of OPENAI_API_KEY
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+
+    if openai_api_key:
+        print(f"OPENAI_API_KEY: {openai_api_key}")
+    else:
+        print("OPENAI_API_KEY not found in .env file")
+
+    return openai_api_key
+
 
 @app.task()
-def greet(name=getuser()):
-    return 'Hello %s!' % name
+def embedding_encode():
+    openai.api_key = read_env_file()
+    
+    openai.Engine.list()  # check we have authenticated
+
+    embedding = openai.Embedding.create(
+        input="product name", model="text-embedding-ada-002")["data"][0]["embedding"]
+    print(embedding)
